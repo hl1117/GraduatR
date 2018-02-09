d54c7a8c23f0a17f4529d0e7d2a66af79c8d2930
Swaraj Bhaduri <subhrobhaduri@pal-nat186-130-15.itap.purdue.edu>
2018-02-08 19:58:47 -0500

    Implement basic communication with Firebase server

--- graduatR/RoleViewController.swift
+++ graduatR/RoleViewController.swift
@@ -2,14 +2,14 @@
 //  RoleViewController.swift
 //  GUI
 //
-//  Created by Dhriti Chawla & Swaraj Bhaduri on 2/8/18.
+//  Created by Dhriti Chawla & SwarajBhaduri on 2/8/18.
 //  Copyright © 2018 Dhriti Chawla. All rights reserved.
 //
 
 import UIKit
 import Firebase
 
-class RoleViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var roleController: UIPickerView!
     var pickerData:  [String] = [String] ()
@@ -30,27 +30,43 @@ class RoleViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
         ref = Database.database().reference()
     }
     
+    // The number of columns of data
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-
+    // The number of rows of data
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return pickerData.count
     }
     
-
+    // The data to return for the row and component (column) that's being passed in
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         value = (row)
-
+        print (value)
         return pickerData[row]
         
         
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     
-
-
+    //Communication with Firebase Database
+    @IBAction func clickNextButton(_ sender: Any) {
+        //Update realtime database based on role
+        self.ref.child("Users").child(pickerData[value]).setValue(["Fname": fname.text, "Lname": lname.text])
+        
+        
+        
+    }
+    
+    
+    
+    
+    
 }
 
