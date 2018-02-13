0e925f84746dd5604e01988067adfe87696fd532
Dhriti Chawla <dhritichawla@pal-nat186-1-214.itap.purdue.edu>
2018-02-13 15:58:21 -0500

    Added GUI for custom acc

---
+++ graduatR/StudentDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StudentDetailViewController.swift
+//  graduatR
+//
+//  Created by Dhriti Chawla on 2/13/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+
+class StudentDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
