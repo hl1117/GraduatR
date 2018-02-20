53a243f1167fa787283e4c7fe66fdfd8336307f0
Harika <harika1117@gmail.com>
2018-02-20 18:27:28 -0500

    started working on profile page and edit profile page

---
+++ GraduatR/graduatR/Controllers/EditProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EditProfileViewController.swift
+//  graduatR
+//
+//  Created by Harika Lingareddy on 2/20/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.tabBarController?.tabBar.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
