76d2c63c3a4fc904dc47d071f2a641465c25cbb7
Simona Virga <simonavirga@pal-nat186-133-33.itap.purdue.edu>
2018-03-08 18:45:40 -0500

    finished adding events to database

--- GraduatR/graduatR/AddEventsViewController.swift
+++ GraduatR/graduatR/AddEventsViewController.swift
@@ -7,29 +7,58 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
 
 class AddEventsViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var startTime: UIDatePicker!
+    @IBOutlet weak var endTime: UIDatePicker!
+    @IBOutlet weak var eventNameTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    var start: NSDate?
+    var end: NSDate?
+    
+    var databaseRef = Database.database().reference()
+    var storageRef = Storage.storage().reference()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addEventButton(_ sender: Any)
+    {
+        
+        self.databaseRef.child("Users").child("Events").child(eventNameTextField.text!).child("Event Name").setValue(eventNameTextField.text)
+        self.databaseRef.child("Users").child("Events").child(eventNameTextField.text!).child("Description").setValue(descriptionTextView.text)
+        getDatesInfo()
+        
+        self.performSegue(withIdentifier: "eventSegue", sender: nil)
     }
-    */
-
+    
+    func getDatesInfo()
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/YYYY  HH:mm"
+        
+        let strDate = dateFormatter.string(from: startTime.date)
+        self.databaseRef.child("Users").child("Events").child(eventNameTextField.text!).child("Start Date").setValue(strDate)
+        //print(strDate)
+        
+        let endDate = dateFormatter.string(from: endTime.date)
+        self.databaseRef.child("Users").child("Events").child(eventNameTextField.text!).child("End Date").setValue(endDate)
+        
+    }
+    
 }
