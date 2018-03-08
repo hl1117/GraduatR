d1f9fab64fca10028559327b8c9270d4a2ec8f47
Harika <harika1117@gmail.com>
2018-03-08 16:06:12 -0500

    VC for events

---
+++ GraduatR/graduatR/CalendarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarViewController.swift
+//  graduatR
+//
+//  Created by Harika Lingareddy on 3/8/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
