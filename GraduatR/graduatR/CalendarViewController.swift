a33f8d78026437c4bd0b3716d9b6aab2f4ca9189
Harika <harika1117@gmail.com>
2018-03-20 18:35:22 -0400

    progress on events page

--- GraduatR/graduatR/CalendarViewController.swift
+++ GraduatR/graduatR/CalendarViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var databaseRef = Database.database().reference()
+    var storageRef = Storage.storage().reference()
+    
+    var eventname = ""
+    var eventdescription = ""
+    var startdate = ""
+    var enddate = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.databaseRef.child("Events").observeSingleEvent(of: .value) {
+            (snapshot: DataSnapshot) in
+            
+            let value = snapshot.value as? [String : AnyObject] ?? [:]
+            
+            if (value["Event Name"] != nil) {
+                self.eventname = (value["Event Name"] as? String!)!
+            }
+            
+            if (value["Event Description"] != nil) {
+                self.eventdescription = (value["Event Description"] as? String!)!
+            }
+            
+            if (value["Start Date"] != nil) {
+                self.startdate = (value["Start Date"] as? String!)!
+            }
+            
+            if (value["End Date"] != nil) {
+                self.enddate = (value["End Date"] as? String!)!
+            }
+            
+            
+            
+        }
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +59,24 @@ class CalendarViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
-    */
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventsCell", for: indexPath) as! EventsCell
+        cell.eventName.text = "hello"
+        return cell
+    }
+    
+    
+    
+    
 
 }
