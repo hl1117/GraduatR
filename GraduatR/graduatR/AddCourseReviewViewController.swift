bc04d8ef52002a357c4682efa4d0e6da336974df
Simona Virga <simonavirga@pal-nat186-140-92.itap.purdue.edu>
2018-03-21 22:26:19 -0400

    added view controllers for course reviews

---
+++ GraduatR/graduatR/AddCourseReviewViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddCourseReviewViewController.swift
+//  graduatR
+//
+//  Created by Simona Virga on 3/21/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+
+class AddCourseReviewViewController: UIViewController {
+
+    @IBOutlet weak var star1: UIButton!
+    @IBOutlet weak var star2: UIButton!
+    @IBOutlet weak var star3: UIButton!
+    @IBOutlet weak var star4: UIButton!
+    @IBOutlet weak var star5: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+    @IBAction func addReviewButton(_ sender: Any)
+    {
+        
+    }
+    
+}
