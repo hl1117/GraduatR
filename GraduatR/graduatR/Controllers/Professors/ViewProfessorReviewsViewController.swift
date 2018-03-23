c4a63c19d358a71f32874b32ccac040f881c8837
Harika <harika1117@gmail.com>
2018-03-23 00:54:28 -0400

    work on prof reviews - comments

--- GraduatR/graduatR/Controllers/Professors/ViewProfessorReviewsViewController.swift
+++ GraduatR/graduatR/Controllers/Professors/ViewProfessorReviewsViewController.swift
@@ -7,24 +7,80 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class ViewProfessorReviewsViewController: UIViewController {
+class ViewProfessorReviewsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    
+    var refresh: UIRefreshControl!
     @IBOutlet weak var nameLabel: UILabel!
     var n = String()
     @IBOutlet weak var tableView: UITableView!
+    
+    var databaseRef = Database.database().reference()
+    var storageRef = Storage.storage().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         nameLabel.text = n
+        
+        refresh = UIRefreshControl()
+        refresh.addTarget(self, action: #selector(CalendarViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refresh, at: 0)
+        
+        
+        
+        self.tableView.reloadData()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        databaseRef.child("ProfessorReviews").child("Comments").observeSingleEvent(of: DataEventType.value, with: { (snapshotA) in
+            if let data = snapshotA.value as? [String: Any] {
+                let values = Array(data.values)
+            }
+        })
+       
+        self.tableView.reloadData()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        //fetch from database
+        
+        self.tableView.reloadData()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.refresh.endRefreshing()
+        
+    }
+    
+    func tableView(_ tableView:UITableView!, numberOfRowsInSection section:Int) -> Int
+    {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView!, cellForRowAt indexPath: IndexPath!) -> UITableViewCell!
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfessorReviewCell", for: indexPath) as! ProfessorReviewCell
+    
+        return cell
     }
     
 
