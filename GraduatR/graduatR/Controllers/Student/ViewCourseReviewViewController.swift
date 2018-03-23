32dfb1c15ae9b52360184ba2b9b2efdc80ac1fd0
Dhriti Chawla <dhritichawla@Dhritis-MacBook-Pro.local>
2018-03-23 00:16:23 -0400

    connections made

---
+++ GraduatR/graduatR/Controllers/Student/ViewCourseReviewViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewCourseReviewViewController.swift
+//  graduatR
+//
+//  Created by Dhriti Chawla on 3/23/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ViewCourseReviewViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [10.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        setChart(dataPoints: months, values: unitsSold)
+        
+    }
+    
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry1 = ChartDataEntry(x: Double(i), y: values[i], data: dataPoints[i] as AnyObject)
+            
+            dataEntries.append(dataEntry1)
+        }
+        print(dataEntries[0].data)
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+    }
+}
