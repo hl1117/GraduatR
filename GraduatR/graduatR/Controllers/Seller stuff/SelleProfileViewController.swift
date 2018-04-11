9d26863039f0e11240a8ae93c0a315cc3bca60bc
Dhriti Chawla <chawla5@purdue.edu>
2018-04-11 16:33:29 -0400

    seller profile GUI

--- GraduatR/graduatR/Controllers/Seller stuff/SelleProfileViewController.swift
+++ GraduatR/graduatR/Controllers/Seller stuff/SelleProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SelleProfileViewController.swift
+//  SellerProfileViewController.swift
 //  graduatR
 //
 //  Created by Swaraj Bhaduri on 4/10/18.
@@ -9,27 +9,16 @@
 import UIKit
 
 class SelleProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var gpa: UILabel!
+    @IBOutlet weak var bio: UILabel!
+    @IBOutlet weak var classesTaken: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
