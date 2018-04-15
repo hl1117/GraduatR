66debee680ad3db1f09496ec6e2592f6ef9e68e1
Swaraj Bhaduri <sbhadur@purdue.edu>
2018-04-15 19:53:08 -0400

    PopOver initial

---
+++ GraduatR/graduatR/PopOverDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopOverDetailsViewController.swift
+//  graduatR
+//
+//  Created by Swaraj Bhaduri on 4/15/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import UIKit
+
+class PopOverDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Popup
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
---
+++ GraduatR/graduatR/Shared.swift
@@ -0,0 +1,9 @@
+//
+//  Shared.swift
+//  graduatR
+//
+//  Created by Swaraj Bhaduri on 4/15/18.
+//  Copyright © 2018 Simona Virga. All rights reserved.
+//
+
+import Foundation
