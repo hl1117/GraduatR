cc11b116621712b67bd321e0996307eb7e287b58
Dhriti Chawla <chawla5@purdue.edu>
2018-04-18 16:43:08 -0400

    tutor logout

--- GraduatR/graduatR/Controllers/Tutor/TutorViewProfileViewController.swift
+++ GraduatR/graduatR/Controllers/Tutor/TutorViewProfileViewController.swift
@@ -77,6 +77,41 @@ class TutorViewProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func clear() {
+        AllVariables.Username = ""
+        AllVariables.Fname = ""
+        AllVariables.Lname = ""
+        AllVariables.GPA = ""
+        AllVariables.standing = ""
+        AllVariables.courses.removeAll()
+        AllVariables.profpic = ""
+        AllVariables.bio = ""
+        AllVariables.uid = ""
+        AllVariables.books.removeAll()
+        AllVariables.courseselected = ""
+        AllVariables.profselected = ""
+        AllVariables.courseratings.removeAll()
+        AllVariables.coursegrade.removeAll()
+        AllVariables.examrating.removeAll()
+        AllVariables.profratings.removeAll()
+        AllVariables.gpaAnon = ""
+    }
+
     @IBAction func logoutButton(_ sender: Any) {
+        
+        if (Auth.auth().currentUser != nil)
+        {
+            do {
+                try? Auth.auth().signOut()
+                
+                if (Auth.auth().currentUser == nil) {
+                    print("USER LOG OUT")
+                    let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as UIViewController
+                    self.present(loginVC, animated: true, completion: nil)
+                    clear()
+                }
+            }
+        }
     }
 }
